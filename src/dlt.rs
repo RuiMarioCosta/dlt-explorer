8a1e058ec533777b5f4c7c18ca030b78c7e0aa44
Rui Costa <rui.ms.costa@ctw.bmwgroup.com>
2025-10-13 18:58:21 +0100

    add DLT parser

--- src/dlt.rs
+++ src/dlt.rs
@@ -1,38 +1,51 @@
 use std::path::PathBuf;
+use std::fs::File;
+use std::io::{BufReader, Read};
+use anyhow::Result;
+use memchr::memmem::Finder;
+
+
+const DLT_DELIMITER: &[u8] = b"DLT";
+
 
 #[derive(Debug)]
 pub struct Dlt<'a> {
     paths: Vec<PathBuf>,
     filter: Option<PathBuf>,
 
-    data: Vec<u8>,
+    // data: Vec<u8>,
     patterns: Vec<&'a str>,
 }
 
 impl<'a> Dlt<'a> {
-    pub fn new(paths: Vec<PathBuf>, filter: Option<PathBuf>) -> Self {
+    pub fn from_files(paths: Vec<PathBuf>, filter: Option<PathBuf>) -> Result<Self> {
         let path = &paths[0];
-        let data = std::fs::read(path).unwrap();
+        // let data = std::fs::read(path).unwrap();
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+        let finder = Finder::new(DLT_DELIMITER);
+        let mut buf = Vec::new();
+        let len = reader.read(&mut buf);
 
-        let _a = &data[..4];
+        // let _a = &data[..100];
 
-        Self {
+        Ok(Self {
             paths,
             filter,
-            data,
+            // data,
             patterns: vec!["asdf"],
-        }
+        })
     }
 
-    pub fn paths(&self) -> &[PathBuf] {
+    pub fn _paths(&self) -> &[PathBuf] {
         &self.paths
     }
 
-    pub fn filter(&self) -> &Option<PathBuf> {
+    pub fn _filter(&self) -> &Option<PathBuf> {
         &self.filter
     }
 
-    pub fn patterns(&self) -> &Vec<&'a str> {
+    pub fn _patterns(&self) -> &Vec<&'a str> {
         &self.patterns
     }
 }
@@ -48,6 +61,8 @@ mod tests {
         path.push(PathBuf::from("src/dlt/tests/testfile_control_messages.dlt"));
         let paths = vec![path];
 
-        let _result = Dlt::new(paths, None);
+        let result = Dlt::from_files(paths, None);
+
+        assert!(result.is_ok());
     }
 }
